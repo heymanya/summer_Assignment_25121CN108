087278351500f1741725c39ef65f32254d55bc5f
Manya Gupta <25121cn108@glbitm.ac.in>
2026-05-29 08:52:45 -0700

    Add files via upload

---
+++ DAY1/Count digits.c
@@ -0,0 +1,18 @@
+//WAP TO COUNT DIGITS IN A NUMBER.
+
+#include<stdio.h>
+int main(){
+    int n, count=0 ;
+
+    printf("Enter a number:");
+    scanf("%d", &n);
+    
+    
+    while(n!=0){
+        n = n/10;
+        count++;
+    }
+    printf("Number of digits are:%d", count);
+
+    return 0;
+}
