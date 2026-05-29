3819a71e18ad0a89826db35794ddbabe7078a494
Manya Gupta <25121cn108@glbitm.ac.in>
2026-05-29 08:09:41 -0700

    Add files via upload

---
+++ multiplication table of a no.c
@@ -0,0 +1,14 @@
+//WAP TO PRINT MULTIPLICATION TABLE OF A GIVEN NUMBER.
+
+#include<stdio.h>
+int main(){
+    int n ;
+
+    printf("Enter the number n:");
+    scanf("%d" , &n);
+
+    for(int i=1 ; i<=10 ; i++){
+        printf("%d * %d = %d\n", n , i , n*i);
+    }
+    return 0;
+}
